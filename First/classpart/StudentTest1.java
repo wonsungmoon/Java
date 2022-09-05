50455694a26c25cd119945c2bd2769eaf365a5af
wonsungmoon <wonsungmoon6@gmail.com>
2022-09-05 17:00:44 +0900

    second commit

---
+++ First/classpart/StudentTest1.java
@@ -0,0 +1,15 @@
+package classpart;
+
+public class StudentTest1 {
+
+    public static void main(String[] args) {
+        Student student1 = new Student(); //첫 번째 학생 생성
+        student1.studentName = "안연수";
+        System.out.println(student1.getStudentName());
+        Student student2 = new Student(); //두 번째 학생 생성
+        student2.studentName = "안승연";
+        System.out.println(student2.getStudentName());
+
+    }
+
+}
