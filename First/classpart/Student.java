d4a39bad529f924d2a6f0ea2d5b33e4e9107daa8
wonsungmoon <wonsungmoon6@gmail.com>
2022-09-07 10:35:33 +0900

    First commit - 2022/09/07

--- First/classpart/Student.java
+++ First/classpart/Student.java
@@ -9,10 +9,14 @@ public class Student {
     public String getStudentName() {
         return studentName;
     }
+    public void setStudentNAme(String name) {
+        studentName = name;
+    }
 
     public static void main(String[] args) {
         Student studentAhn = new Student();
-        studentAhn.studentName = "안연수";
+        studentAhn.setStudentNAme("안연수");
+        //studentAhn.studentName = "안연수"; 위 코드와 실행결과는 같음.ㄴ
 
         System.out.println(studentAhn.studentName);
         System.out.println(studentAhn.getStudentName());
