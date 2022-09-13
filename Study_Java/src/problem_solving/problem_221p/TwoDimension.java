d938f13c442e39a7421a4766c669e163513fdfbc
wonsungmoon <wonsungmoon6@gmail.com>
2022-09-13 09:16:02 +0900

    First commit - 2022/09/13

---
+++ Study_Java/src/problem_solving/problem_221p/TwoDimension.java
@@ -0,0 +1,13 @@
+package src.problem_solving.problem_221p;
+
+public class TwoDimension {
+    public static void main(String[] args) {
+
+        char[][] arr = {{'a', 'b'}, {'c', 'd'}, {'e', 'f'}, {'g', 'h'}, {'i', 'j'}, {'k', 'l'},
+                {'m', 'n'}, {'o', 'p'}, {'q', 'r'}, {'s', 't'}, {'u', 'v'}, {'w', 'x'}, {'y', 'z'}};
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
