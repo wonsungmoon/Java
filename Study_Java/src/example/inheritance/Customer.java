386803bef5fdcfc7d4be9e554ddbe9b20e689576
wonsungmoon <wonsungmoon6@gmail.com>
2022-09-13 13:13:29 +0900

    Third commit - 2022/09/13

--- Study_Java/src/example/inheritance/Customer.java
+++ Study_Java/src/example/inheritance/Customer.java
@@ -1,9 +1,9 @@
 package src.example.inheritance;
 
 public class Customer {
-    private int customerID; //고객 아이디
-    private String customerName; //고객 이름
-    private String customerGrade; //고객 등급
+    protected int customerID; //고객 아이디
+    protected String customerName; //고객 이름
+    protected String customerGrade; //고객 등급
     int bonusPoint; //보너스 포인트
     double bonusRatio; //적립 비율
     //멤버 변수
@@ -20,4 +20,29 @@ public class Customer {
     public String showCustomerInfo() {
         return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는" + bonusPoint + "입니다.";
     } //고객 정보를 반환하는 메서드
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
+    }
+    //protected 예약어로 선언한 변수를 외부에서 사용할 수 있도록 get(), set() 메서드 추가
 }
