0694697810b7ea53f1c56cbbf406f767147dc836
wonsungmoon <wonsungmoon6@gmail.com>
2022-09-19 17:06:58 +0900

    Second commit - 2022/09/19

---
+++ Study_Java/src/example/object/ToStringEx.java
@@ -0,0 +1,25 @@
+package src.example.object;
+
+class Book {
+    int bookNumber;
+    String bookTitle;
+
+    Book(int bookNumber, String bookTitle) {
+        this.bookNumber = bookNumber;
+        this.bookTitle = bookTitle;
+    }
+
+    @Override
+    public String toString() {
+        return bookTitle + "," + bookNumber;
+    } //toString() 메서드 재정의
+}
+
+public class ToStringEx {
+    public static void main(String[] args) {
+        Book book1 = new Book(200, "개미");
+
+        System.out.println(book1);
+        System.out.println(book1.toString());
+    }
+}
