3b5da6edf3bf3452131ae79946f73a20abd679b6
wonsungmoon <wonsungmoon6@gmail.com>
2022-10-08 22:53:55 +0900

    First commit - 2022/10/8

--- Study_Java/src/assignment/Test.java
+++ Study_Java/src/assignment/Test.java
@@ -1,8 +1,21 @@
 package src.assignment;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
 
+        int[] nums = { 3, 1, 5, 4, 2 };
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length - i - 1; j++) {
+                if(nums[j] > nums[j+1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        System.out.println("버블정렬 : " + Arrays.toString(nums));
     }
 }
 
--- Study_Java/src/practise/TestABC.java
+++ Study_Java/src/practise/TestABC.java
@@ -4,7 +4,9 @@ public class TestABC {
     public static void main(String[] args) {
         C c = new C();
         B b = new B();
+        A a = (A)new B();
         System.out.println(c.add(1,2));
         System.out.println(b.add(1,2));
+        System.out.println(a.add(1,2));
     }
 }
