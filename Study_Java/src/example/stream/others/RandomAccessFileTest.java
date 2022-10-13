dca5072117f25ec91f2d1f4c3e32d77f660e8b02
wonsungmoon <wonsungmoon6@gmail.com>
2022-10-13 21:53:29 +0900

    First commit - 2022/10/13

--- Study_Java/src/example/stream/others/RandomAccessFileTest.java
+++ Study_Java/src/example/stream/others/RandomAccessFileTest.java
@@ -7,17 +7,21 @@ public class RandomAccessFileTest {
     public static void main(String[] args) throws IOException {
         RandomAccessFile rf = new RandomAccessFile("random.txt", "rw");
         rf.writeInt(100);
-        System.out.println("파일 포인터 위치:" + rf.getFilePointer()); //파일 포인터 위치를 반환하는 메서드
+        System.out.println("파일 포인터 위치:" + rf.getFilePointer());
         rf.writeDouble(3.14);
         System.out.println("파일 포인터 위치:" + rf.getFilePointer());
         rf.writeUTF("안녕하세요");
         System.out.println("파일 포인터 위치:" + rf.getFilePointer());
 
+        rf.seek(0);
+        System.out.println("파일 포인터 위치:" + rf.getFilePointer()); //파일 포인터 위치를 맨 처음으로 옮기고 위치를 출력함
+
         int i = rf.readInt();
         double d = rf.readDouble();
         String str = rf.readUTF();
 
-        System.out.println("파일 포인터 위치:" + rf.getFilePointer());
+        System.out.println("파일 포인터 위치:" + rf.getFilePointer()); //읽기가 끝난 후 파일 포인터 위치를 출력함
+        
         System.out.println(i);
         System.out.println(d);
         System.out.println(str);
