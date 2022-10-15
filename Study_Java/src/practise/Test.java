92a8b96d845449669c6cee0b822394c16f1265f6
wonsungmoon <wonsungmoon6@gmail.com>
2022-10-15 22:27:27 +0900

    First commit - 2022/10/15

--- Study_Java/src/practise/Test.java
+++ Study_Java/src/practise/Test.java
@@ -2,14 +2,33 @@ package src.practise;
 
 public class Test {
     public static void main(String[] args){
-
-        int alpha = 65;
-
-        for(int i = 0; i < 27; i ++) {
-            System.out.print((char)alpha);
-            alpha++;
+        class Ramen{
         }
+        class Water{
+        }
+        class Pot{
+            Ramen ramen;
+            Water water;
+            boolean boil = false;
+            boolean sauce = false;
+            public Pot(Ramen ramen, Water water) {
+                this.ramen = ramen;
+                this.water = water;
+            }
+            public void addSauce() {
+                this.sauce = true;
+            }
+        }
+        Ramen ramen = new Ramen();
+        Water water = new Water();
+        Pot pot = new Pot(ramen,water);
+        if(pot.boil != true) {
+            System.out.println("기다린다.");
+        }
+        pot.addSauce();
+        if(pot.sauce != true) {
+            System.out.println("기다린다.");
+        }
+        System.out.println("맛있게 먹는다");
     }
 }
-
-
